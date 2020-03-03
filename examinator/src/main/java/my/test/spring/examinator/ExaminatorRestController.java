e2dfa55ef7f5d9e899e1fda49899b038f466baeb
jeak110 <jeak110@gmail.com>
2020-03-03 19:29:32 +0700

    lb: rest template @LoadBalanced

--- examinator/src/main/java/my/test/spring/examinator/ExaminatorRestController.java
+++ examinator/src/main/java/my/test/spring/examinator/ExaminatorRestController.java
@@ -4,8 +4,12 @@ import com.netflix.discovery.EurekaClient;
 import my.test.spring.examinator.model.Exercise;
 import my.test.spring.examinator.section.Section;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.cloud.client.ServiceInstance;
 import org.springframework.cloud.client.discovery.DiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.cloud.netflix.eureka.http.RestTemplateTransportClientFactory;
+import org.springframework.context.annotation.Bean;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
@@ -22,6 +26,8 @@ import static java.util.Arrays.asList;
 public class ExaminatorRestController {
     @Autowired
     private DiscoveryClient discoveryClient;
+    @Autowired
+    private RestTemplate restTemplate;
 
     public ExaminatorRestController() {
     }
@@ -34,15 +40,14 @@ public class ExaminatorRestController {
         for (Section section : sections) {
             String counterStr = map.get(section.getName());
             Integer counter = counterStr != null ? Integer.parseInt(counterStr) : 2;
-            List<Exercise> exercises = getExercisesForSection(counter, section.getBaseUrl());
+            List<Exercise> exercises = getExercisesForSection(counter, section.getName());
             result.addAll(exercises);
         }
         return result;
     }
 
-    private List<Exercise> getExercisesForSection(Integer counter, String baseUrl) {
-        RestTemplate restTemplate = new RestTemplate();
-        Exercise[] arr = restTemplate.getForObject("http://" + baseUrl + "/exercise/random?counter=" + counter, Exercise[].class);
+    private List<Exercise> getExercisesForSection(Integer counter, String serviceId) {
+        Exercise[] arr = restTemplate.getForObject("http://" + serviceId + "/exercise/random?counter=" + counter, Exercise[].class);
         return asList(arr);
     }
 
@@ -53,4 +58,8 @@ public class ExaminatorRestController {
                 .map(instance -> new Section(instance.getServiceId().toLowerCase(), instance.getHost() + ":" + instance.getPort()))
                 .collect(Collectors.toList());
     }
-}
+
+    @Bean @LoadBalanced
+    public RestTemplate restTemplate(RestTemplateBuilder builder) {
+        return builder.build();
+    }}
--- math-service/src/main/java/my/test/spring/mathservice/ExerciseController.java
+++ math-service/src/main/java/my/test/spring/mathservice/ExerciseController.java
@@ -2,6 +2,8 @@ package my.test.spring.mathservice;
 
 import my.test.spring.mathservice.model.Exercise;
 import org.apache.el.stream.Stream;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.ApplicationContext;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -13,8 +15,14 @@ import java.util.List;
 @RestController
 @RequestMapping("/exercise")
 public class ExerciseController {
+    @Autowired
+    private ApplicationContext appContext;
+
+
     @GetMapping("/random")
     public List<Exercise> random(@RequestParam Integer counter) {
+        System.out.println("Math: " + appContext.getId());
+
         List<Exercise> result = new ArrayList<>();
         for (int i = 0; i < counter; i++) {
             result.add(generateExercise());
